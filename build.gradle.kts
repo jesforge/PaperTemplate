2b68519d6bb94abbda3ae787cf9abf2b2eccaaf3
Jesper <jesper.l.richert@gmail.com>
2024-12-04 20:27:14 +0100

    Publish Template

---
+++ build.gradle.kts
@@ -0,0 +1,70 @@
+plugins {
+    kotlin("jvm") version "2.1.0"
+    id("com.gradleup.shadow") version "9.0.0-beta2"
+    id("io.papermc.paperweight.userdev") version "1.7.5"
+    id("xyz.jpenilla.run-paper") version "2.3.1"
+    id("net.minecrell.plugin-yml.paper") version "0.6.0"
+    kotlin("plugin.serialization") version "2.1.0"
+}
+
+val mcVersion = properties["minecraftVerions"] as String
+val projectVersion = properties["version"] as String
+val projectName = properties["name"] as String
+val groupID = properties["group"] as String
+val mainClass = properties["main"] as String
+val projectDescription = properties["description"] as String
+val twilightVersion = properties["twilightVersion"] as String
+val commandAPIVersion = properties["commandAPIVersion"] as String
+val adventureAPIVersion = properties["adventureAPIVersion"] as String
+
+group = groupID
+version = projectVersion
+
+repositories {
+    mavenCentral()
+    maven("https://repo.papermc.io/repository/maven-public/")
+    maven("https://repo.flyte.gg/releases")
+}
+
+dependencies {
+    // Kotlin
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.3")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
+    // Paper
+    paperweight.paperDevBundle("${mcVersion}-R0.1-SNAPSHOT")
+
+    // Adventure
+    implementation("net.kyori:adventure-api:${adventureAPIVersion}")
+
+    // Twilight
+    implementation("gg.flyte:twilight:${twilightVersion}")
+
+    // Command API
+    compileOnly("dev.jorel:commandapi-bukkit-core:${commandAPIVersion}")
+    implementation("dev.jorel:commandapi-bukkit-shade-mojang-mapped:${commandAPIVersion}")
+    implementation("dev.jorel:commandapi-bukkit-kotlin:${commandAPIVersion}")
+}
+
+kotlin {
+    jvmToolchain(21)
+}
+
+tasks.build {
+    dependsOn(tasks.shadowJar)
+    dependsOn(tasks.reobfJar)
+}
+
+tasks {
+    runServer {
+        minecraftVersion(mcVersion)
+    }
+}
+
+paper {
+    name = projectName
+    version = version
+    description = projectDescription
+    main = mainClass
+    authors = listOf("xyzjesper")
+}
---
+++ src/main/kotlin/dev/xyzjesper/papertemplate/config/ConfigManager.kt
@@ -0,0 +1,16 @@
+package dev.xyzjesper.papertemplate.config
+
+import kotlinx.serialization.encodeToString
+import java.io.File
+
+object ConfigManager {
+
+    private val settingsFile = File("plugins/AffiliateCode/config.json")
+
+    val settings = settingsFile.loadConfig(SettingsData)
+
+    fun save() {
+        settingsFile.writeText(json.encodeToString(settings))
+    }
+
+}
