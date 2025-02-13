88945018cffda3a691932f4cd070fc4ca606110c
Jesper <94371018+jesforge@users.noreply.github.com>
2025-02-13 17:24:00 +0100

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,9 @@
 plugins {
-    kotlin("jvm") version "2.1.0"
-    id("com.gradleup.shadow") version "9.0.0-beta2"
+    kotlin("jvm") version "2.0.20-Beta1"
+    id("com.gradleup.shadow") version "9.0.0-beta8"
     id("io.papermc.paperweight.userdev") version "1.7.5"
     id("xyz.jpenilla.run-paper") version "2.3.1"
-    id("net.minecrell.plugin-yml.paper") version "0.6.0"
+    id("de.eldoria.plugin-yml.bukkit") version "0.7.0"
     kotlin("plugin.serialization") version "2.1.0"
 }
 
